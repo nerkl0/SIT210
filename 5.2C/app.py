12e7e796ea5b79e4ff959d8f16c134eeab43c3c7
Nicole <nicolebirchh@gmail.com>
2026-04-12 13:41:46 +1000

    added some extra notes

--- 5.2C/app.py
+++ 5.2C/app.py
@@ -6,11 +6,14 @@ rooms = {
     "Closet": 19
 }
 
+# assigns each value in rooms to it's respective GPIO PWM enabled pin
 leds = {room: PWMOutputDevice(pin) for room, pin in rooms.items()}
 
+# power toggle for LEDs
 def toggle(room, is_on):
     leds[room].value = 1 if is_on else 0
 
+# uses PWM module .value input to set Duty Cycle of LED
 def dimmer(room, value):
     leds[room].value = value * 0.01
 
