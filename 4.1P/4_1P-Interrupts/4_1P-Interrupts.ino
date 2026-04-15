3c401b8f5b6c25d9220452f21bf1392a7f4b04b3
Nicole <nicolebirchh@gmail.com>
2026-04-15 22:08:22 +1000

    changed motion sensor polling to interrupt

--- 4.1P/4_1P-Interrupts/4_1P-Interrupts.ino
+++ 4.1P/4_1P-Interrupts/4_1P-Interrupts.ino
@@ -8,12 +8,20 @@ const uint8_t SWITCH_PIN = 3;
 const uint8_t LEDB_PIN = 7;
 const uint8_t LEDR_PIN = 8;
 
-const unsigned long DEBOUNCE_MS = 200;
-volatile bool isrActive = false;
-volatile unsigned long lastTrigger = 0;
+const unsigned int DEBOUNCE_MS = 300;
+const unsigned int MOTION_TIMEOUT = 10000; 
+volatile bool switchActive = false;
+volatile unsigned long lastMotionTrigger = 0; 
+volatile unsigned long lastSwitchTrigger = 0;
 volatile byte ledState = LOW;
 
 const float LIGHT_THRESHOLD = 20;
+
+unsigned long lastLog = 0;
+const unsigned int LOG_INTERVAL = 2000;
+float lastLux = -1;
+bool lastMotionState = false;
+bool lastLedState = LOW;
 
 void triggerLights(bool power){
   ledState = power; 
@@ -27,29 +35,36 @@ void triggerLights(bool power){
 // Debounce logic ignores a retrigger with DEBOUNCE_MS to prevent any potential flickering of the leds
 void ISRtriggerLights(){
   unsigned long now = millis();
-  if (now - lastTrigger < DEBOUNCE_MS) return;
-  lastTrigger = now;
-  
-  if (isrActive) {
-    isrActive = false;
-    triggerLights(LOW); 
-  } else {
-    isrActive = true;
-    triggerLights(HIGH);
-  }
+  if (now - lastSwitchTrigger < DEBOUNCE_MS) return;
+  lastSwitchTrigger = now;
+
+  switchActive = !switchActive;
+  log(switchActive ? "== Switch ON ==" : "== Switch OFF ==");
+
+}
+
+void ISRmotionDetected(){
+  lastMotionTrigger = millis();  
 }
 
 void log(const char* message){
   Serial.println(message);
   Serial.println();
 }
-void log(float lux, bool motionDetected){
+void logLux(float lux){    
   Serial.print("Lux: ");    
   Serial.println(lux);
-  Serial.print("Motion: "); 
-  Serial.println(motionDetected);
   Serial.print("LED: ");    
   Serial.println(ledState);
+  Serial.println();
+}
+void logMotion(bool motionSensor){
+  Serial.print("Motion Detected: ");
+  Serial.println(motionSensor);
+  if(motionSensor) {
+    Serial.print("Time Remaining: ");
+    Serial.println(MOTION_TIMEOUT - (millis() - lastMotionTrigger));
+  }
   Serial.println();
 }
 
@@ -61,32 +76,51 @@ void setup() {
   lightSensor.begin();
 
   pinMode(SWITCH_PIN, INPUT);
-  pinMode(SENSOR_PIN, INPUT_PULLUP);
+  pinMode(SENSOR_PIN, INPUT);
   pinMode(LEDR_PIN, OUTPUT);
   pinMode(LEDB_PIN, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), ISRtriggerLights, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), ISRmotionDetected, RISING);
 
   // warm up delay to give the PIR sensor time to adjust to the room conditions
   Serial.println("Sensor warming up...");
-  delay(60000);
+  //delay(60000);
   Serial.println("Warmup complete");
 }
 
 void loop() {
-  // doesn't let sensors override if the switch has been turrned on 
-  if (isrActive) {
+  // won't let sensors override if the switch has been turrned on 
+  if (switchActive) {
+    if (ledState == LOW) {
+      triggerLights(true);
+    }
+    return;
+  }
+  
+  float lux = lightSensor.readLightLevel(); 
+  bool motionTimeout = (millis() - lastMotionTrigger) < MOTION_TIMEOUT;
+  unsigned long now = millis();
+
+  if (lux != lastLux || ledState != lastLedState || now - lastLog > LOG_INTERVAL) {
+    lastLux = lux;
+    lastMotionState = motionTimeout;
+    lastLedState = ledState;
+    lastLog = now;
+    logLux(lux);
+    logMotion(motionTimeout);
+  }
+
+  // If light levels are high, make sure lights are off then exit loop early
+  if (lux > LIGHT_THRESHOLD){
+    if (ledState == HIGH)
+      triggerLights(false);
     return;
   }
 
-  float lux = lightSensor.readLightLevel(); 
-  uint8_t motion = digitalRead(SENSOR_PIN);
-  bool lowLight = lux < LIGHT_THRESHOLD;
+  if (!motionTimeout && ledState == HIGH)
+    triggerLights(false);
+  else if (motionTimeout && ledState == LOW)
+    triggerLights(true);
 
-  log(lux, motion);
-
-  // if no motion, irrespective of light levels. Lights are turned off.
-  // otherwise if low level light and motion detected, lights are on. 
-  if (!motion && ledState == HIGH) triggerLights(LOW);
-  else if (ledState == LOW && (lowLight && motion)) triggerLights(HIGH);
 }
