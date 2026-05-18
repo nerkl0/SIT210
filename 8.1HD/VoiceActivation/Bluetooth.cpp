c73ada1d05d3319b4bdbeee61e9447c970032288
Nicole <nicolebirchh@gmail.com>
2026-05-18 21:49:45 +1000

    added comments

--- 8.1HD/VoiceActivation/Bluetooth.cpp
+++ 8.1HD/VoiceActivation/Bluetooth.cpp
@@ -1,5 +1,6 @@
 #include "Bluetooth.h"
 
+// Setup BT service, characteristic UUID must match the sender UUID
 BLEService lightService("88d6aab0-34ef-44dc-87c9-a2b070969a46");
 BLEStringCharacteristic ble_request("5fcc86b2-1b9a-4316-8845-98c6ec20a1bc", BLERead | BLEWrite, 20);
 
