3d1b429f2c4829ed1f7b1036120958d7ee362ad3
Nicole <nicolebirchh@gmail.com>
2026-06-04 13:02:43 +1000

    Added contextual comments

--- 11.1/src/TempSensor.cpp
+++ 11.1/src/TempSensor.cpp
@@ -4,35 +4,40 @@
 static OneWire oneWire(TEMP_SENSOR_PIN);
 static DallasTemperature sensors(&oneWire);
 static float valid_temp_read = 0; 
-static unsigned long bad_read_start_ms = 0;
+static unsigned long bad_read_start = 0;
 static const unsigned long change_state_timeout = 5000; 
 
 TempSensorState temp_sensor_state = OK; 
 
 void tempSensor_begin(){
+    // Converts sensor read into non-blocking function. By default library will block waiting for conversion (750ms)
     sensors.setWaitForConversion(false);
     sensors.begin();
     sensors.requestTemperatures();
 }
 
+// Returns value read by temperature sensor. On first detection of a bad read (DEVICE_DISCONNECTED_C), 
+// start bad read timer (bad_read_start). Gives the sensor time to correct single bad reads. Only updates 
+// sensor state if bad reads exceed set timeout (change_state_timeout).
+// During this period the last valid temperature reading is sent back to the main program to maintain state
 float tempSensor_read() {
-    float temp = sensors.getTempCByIndex(0);
-    sensors.requestTemperatures();
+    float temp = sensors.getTempCByIndex(0); // reads the previous conversion
+    sensors.requestTemperatures(); // kicks off next conversion
 
     if (temp == DEVICE_DISCONNECTED_C) {
-        if (bad_read_start_ms == 0) 
-            bad_read_start_ms = millis();
-        else if ((millis() - bad_read_start_ms) >= change_state_timeout) 
+        if (bad_read_start == 0) 
+            bad_read_start = millis();
+        else if ((millis() - bad_read_start) >= change_state_timeout) 
             temp_sensor_state = BAD;
-        Serial.println("TEMP: Bad read");
     } else {
-        bad_read_start_ms = 0;
+        bad_read_start = 0;
         temp_sensor_state = OK;
         valid_temp_read = temp;
     }
     return valid_temp_read;
 }
 
+// Getter
 TempSensorState tempSensor_state(){
     return temp_sensor_state;
 }
